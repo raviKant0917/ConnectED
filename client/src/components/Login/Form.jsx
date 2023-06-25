7beb2f13f96f44dbaf1e2b7ea3e8b57a229f6530
Piyush@laptop <chandra7piyush@gmail.com>
2023-06-26 02:04:09 +0530

    small changes

--- client/src/components/Login/Form.jsx
+++ client/src/components/Login/Form.jsx
@@ -1,13 +1,87 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Form = () => {
+    const Navigate = useNavigate();
+    const [details, setDetails] = useState({ email: "", password: "" });
+    const [check, setCheck] = useState({ email: false, password: false });
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        if (details.email === "") {
+            setCheck((t) => {
+                return { ...t, email: true };
+            });
+        }
+        if (details.password === "") {
+            setCheck((t) => {
+                return { ...t, password: true };
+            });
+        }
+        if (details.email === "" || details.password === "") {
+            return;
+        }
+
+        const res = await fetch("http://localhost:8000/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: details.email,
+                password: details.password,
+            }),
+        });
+
+        const response = await res.json();
+        console.log(response);
+        if (response.status === "success") {
+            Navigate("/");
+        }
+    };
+
     return (
-        <form>
-            <label htmlFor="">Email</label>
-            <input type="email" placeholder="Type your Email" />
-            <label htmlFor="">Password</label>
-            <input type="password" placeholder="Type your Password" />
+        <form onSubmit={submitHandler}>
+            <label htmlFor="">
+                Email&nbsp;
+                {check.email && (
+                    <span style={{ color: "red" }}>Email can't be empty</span>
+                )}
+            </label>
+            <input
+                type="email"
+                value={details.email}
+                onInput={(e) => {
+                    setDetails((t) => {
+                        return { ...t, email: e.target.value };
+                    });
+                    setCheck((t) => {
+                        return { ...t, email: false };
+                    });
+                }}
+                placeholder="Type your Email"
+                autoComplete="auto"
+            />
+            <label htmlFor="">
+                Password&nbsp;
+                {check.password && (
+                    <span style={{ color: "red" }}>
+                        Password can't be empty
+                    </span>
+                )}
+            </label>
+            <input
+                type="password"
+                value={details.password}
+                onInput={(e) =>
+                    setDetails((t) => {
+                        return { ...t, password: e.target.value };
+                    })
+                }
+                placeholder="Type your Password"
+                autoComplete="auto"
+            />
             <input type="submit" />
             <Link to={"/"} style={{ color: "skyblue" }}>
                 Forget Password?

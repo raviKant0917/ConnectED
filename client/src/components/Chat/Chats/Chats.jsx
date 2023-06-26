946b3f221f9efc89d229028e33dcb4e78e99ac38
ravikant0917 <21bcs108@nith.ac.in>
2023-06-26 11:25:51 +0530

    jwtAuth

--- client/src/components/Chat/Chats/Chats.jsx
+++ client/src/components/Chat/Chats/Chats.jsx
@@ -1,73 +1,73 @@
-import React, { useEffect, useState } from "react";
-import { BiSend } from "react-icons/bi";
-import Profile from "./Profile";
-import "./Chats.scss";
-import {
-    addDoc,
-    collection,
-    serverTimestamp,
-    onSnapshot,
-    query,
-    where,
-} from "firebase/firestore";
-import { db } from "../../firebase";
-import { useSelector } from "react-redux/es/hooks/useSelector";
+// import React, { useEffect, useState } from "react";
+// import { BiSend } from "react-icons/bi";
+// import Profile from "./Profile";
+// import "./Chats.scss";
+// import {
+//     addDoc,
+//     collection,
+//     serverTimestamp,
+//     onSnapshot,
+//     query,
+//     where,
+// } from "firebase/firestore";
+// import { db } from "../../firebase";
+// import { useSelector } from "react-redux/es/hooks/useSelector";
 
-const Chats = ({ obj, user, data, set, room, ms }) => {
-    const [chat, setChat] = useState("");
-    const userChatsRef = collection(db, "userChats");
-    const name = useSelector((state) => state.user.name);
+// const Chats = ({ obj, user, data, set, room, ms }) => {
+//     const [chat, setChat] = useState("");
+//     const userChatsRef = collection(db, "userChats");
+//     const name = useSelector((state) => state.user.name);
 
-    // useEffect(() => {
-    //     const queryMessages = query(userChatsRef, where("room", "==", room));
-    //     const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
-    //         let messages = [];
-    //         snapshot.forEach((doc) => {
-    //             messages.push({ ...doc.data(), id: doc.id });
-    //         });
-    //         ms(messages);
-    //     });
+//     // useEffect(() => {
+//     //     const queryMessages = query(userChatsRef, where("room", "==", room));
+//     //     const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+//     //         let messages = [];
+//     //         snapshot.forEach((doc) => {
+//     //             messages.push({ ...doc.data(), id: doc.id });
+//     //         });
+//     //         ms(messages);
+//     //     });
 
-    //     return () => unsubscribe();
-    // }, []);
+//     //     return () => unsubscribe();
+//     // }, []);
 
-    const clickHandler = async () => {
-        if (chat === "") return;
-        await addDoc(userChatsRef, {
-            text: chat,
-            createdAt: serverTimestamp(),
-            user: name,
-            room,
-        });
-        setChat("");
-    };
+//     const clickHandler = async () => {
+//         if (chat === "") return;
+//         await addDoc(userChatsRef, {
+//             text: chat,
+//             createdAt: serverTimestamp(),
+//             user: name,
+//             room,
+//         });
+//         setChat("");
+//     };
 
-    return (
-        <div className="chats">
-            <Profile data={data} set={set} user={user} />
+//     return (
+//         <div className="chats">
+//             <Profile data={data} set={set} user={user} />
 
-            <div className="chat-wrapper">
-                {obj.chats.map((obj) => (
-                    <div
-                        className={`chat ${obj.send === user ? "user" : "you"}`}
-                    >
-                        <div> {obj.text}</div>
-                    </div>
-                ))}
-            </div>
+//             <div className="chat-wrapper">
+//                 {obj.chats.map((obj) => (
+//                     <div
+//                         className={`chat ${obj.send === user ? "user" : "you"}`}
+//                     >
+//                         <div> {obj.text}</div>
+//                     </div>
+//                 ))}
+//             </div>
 
-            <div className="text-input">
-                <input
-                    type="text"
-                    placeholder="Type your message"
-                    value={chat}
-                    onInput={(e) => setChat(e.target.value)}
-                />
+//             <div className="text-input">
+//                 <input
+//                     type="text"
+//                     placeholder="Type your message"
+//                     value={chat}
+//                     onInput={(e) => setChat(e.target.value)}
+//                 />
 
-                <BiSend onClick={clickHandler} />
-            </div>
-        </div>
-    );
-};
+//                 <BiSend onClick={clickHandler} />
+//             </div>
+//         </div>
+//     );
+// };
 
-export default Chats;
+// export default Chats;

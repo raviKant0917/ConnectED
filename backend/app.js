fd5bda627a4e2130c54b3fb0c84ff743963d627e
ravikant0917 <21bcs108@nith.ac.in>
2023-06-26 17:39:26 +0530

    cors2

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "https://connect-fnyb.onrender.com/",
+        origin: '*',
         credentials: true,
     })
 );
